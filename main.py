8f72e7370ddcda3115b3e407faa6016db7803a03
chris8762 <jasa.topors@gmail.com>
2025-04-03 19:55:51 +0000

    commit

--- main.py
+++ main.py
@@ -19,9 +19,40 @@ def loginOrRegister():
 def randomRecipe():
     return render_template("randomRecipe.html")
 
+
+#dost za dones prisel sem na limit za API
 @app.route("/getRandomRecipe")
 def getRandomRecipe():
-    return "x"
+    sestavine = request.args.get("sestavine")
+    vrsta = request.args.get("vrsta")
+    url = f"https://api.spoonacular.com/recipes/complexSearch?query={sestavine}&cuisine={vrsta}&apiKey=32be0b921f2a4e7fac0f85279c03b8cb"
+
+    call = requests.get(url).json()
+    get_recept = random.choice(call["results"])
+    id_recepta = get_recept["id"]
+
+    recept_url = f"https://api.spoonacular.com/recipes/{id_recepta}/information?apiKey=32be0b921f2a4e7fac0f85279c03b8cb"
+
+    recept = requests.get(recept_url).json()
+
+    ing = []
+    amount = []
+    unit = []
+    for ingredient in recept["extendedIngredients"]:
+        if "nameClean" in ingredient:
+            amount.append(ingredient["original"])
+
+
+    print(ing,amount )
+
+    return jsonify({"slika": f"<img src={recept["image"]}>",
+                    "ime": recept["title"],
+                    "ingredients": amount,
+                    "navodila": recept["instructions"]})
+
+
+
+
 
 @app.route("/getHladilnik")
 def getHladilnik():
